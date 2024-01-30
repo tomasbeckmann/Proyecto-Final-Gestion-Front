e58df43d4d75486100019a9b2ba056a3f8739e97
fjparrah <48214359+fjparrah@users.noreply.github.com>
2024-01-30 23:42:30 +0000

    actualizacion

---
+++ src/components/landingpage/Team.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Conoce a nuestro equipo</h2>
+          <p>
+            Los mejores profesionales a tu disposicion para el manejo de tus datos
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4 col-sm-1 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img" />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
