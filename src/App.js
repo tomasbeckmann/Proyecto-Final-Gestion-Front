e6b44a830b75d52f36ba31d08a0bd5a4ad33876d
fjparrah <48214359+fjparrah@users.noreply.github.com>
2024-01-31 15:01:27 +0000

    registro avance 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Landing } from "./views/landing";
+import Register from "./views/register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing/>} /> {/* 👈 Renders at /app/ */}
+        <Route path="/register" element={<Register/>} /> {/* 👈 Renders at /app/ */}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/views/register.js
@@ -0,0 +1,13 @@
+import Registers from "../components/application/register";
+
+const Register = () => {
+
+    return (
+        <div>
+        <Registers />
+        </div>    
+    )
+
+}
+
+export default Register; 
