01c0206fd9d7a3bdc517fd104891a13953672c39
Tomás Beckmann <147322170+tomasbeckmann@users.noreply.github.com>
2024-01-31 00:38:31 +0000

    updated

---
+++ src/views/landing.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { Navigation } from "./components/landingpage/navigation";
+import { Header } from "./components/landingpage/header";
+import { Features } from "./components/landingpage/features";
+import { About } from "./components/landingpage/about";
+import { Services } from "./components/landingpage/services";
+// import { Gallery } from "./components/landingpage/gallery";
+import { Testimonials } from "./components/landingpage/testimonials";
+import { Team } from "./components/landingpage/Team";
+// import { Contact } from "./components/landingpage/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const Landing = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      {/* <Gallery data={landingPageData.Gallery} /> */}
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      {/* <Contact data={landingPageData.Contact} /> */}
+    </div>
+  );
+};
+
+export default Landing;
