4122451e25ca94647bcf3ed1268585e5d2854855
Deyruby <128061593+Deyruby@users.noreply.github.com>
2024-01-31 16:01:35 +0000

    login actualizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import { Landing } from "./views/landing";
+import { Home } from "./views/home";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} /> {/* 👈 Renders at /app/ */}
+        <Route path="/" element={<Landing/>} /> {/* 👈 Renders at /app/ */}
       </Routes>
     </BrowserRouter>
   );
--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,14 @@
+import Login from "../components/application/login"
+
+
+
+
+
 export const Home = () => {
     return (
+        <>
         <h1>este es mi home </h1>
+        <Login/>
+        </>
     )
 }
