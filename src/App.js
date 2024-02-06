01d390c2afbd23c18ccd481f7b7194fade356a38
Tomás Beckmann <147322170+tomasbeckmann@users.noreply.github.com>
2024-02-06 14:50:05 +0000

    avanzado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Landing } from "./views/landing";
+import { FileFolder } from "./views/filefolder";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} /> {/* 👈 Renders at /app/ */}
+        <Route path="/filefolder" element={<FileFolder />} /> {/* 👈 Renders at /app/ */}
       </Routes>
     </BrowserRouter>
   );
